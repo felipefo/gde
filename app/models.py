9465d1d98e9fdb55acb26da5eb54ba5a60782d0f
André Spadetto Leão <andre.sp.leao@gmail.com>
2016-07-12 14:41:59 -0300

    Validacao campos vazios.

--- app/models.py
+++ app/models.py
@@ -4,19 +4,19 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=20, null=False, unique=True)
+    nome = models.CharField(max_length=20, null=False, blank=False, unique=True)
 
     def __str__(self):
         return self.nome
 
 class Setor(models.Model):
-    nome = models.CharField(max_length=20, null=False, unique=True)
-    sigla = models.CharField(max_length=20, null=False, unique=True)
-    funcao = models.CharField(max_length=250, null=False, unique=True)
+    nome = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    sigla = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    funcao = models.CharField(max_length=250, null=False, blank=False, unique=True)
 
 
     def __str__(self):
-        nome = models.CharField(max_length=20, null=False, unique=True)
+        nome = models.CharField(max_length=20, null=False, blank=False, unique=True)
         return self.nome
 
 

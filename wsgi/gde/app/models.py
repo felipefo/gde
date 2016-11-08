15b54ed3548b92f0ba4ede877d9c53e8a403f6d5
André Spadetto Leão <andre.sp.leao@gmail.com>
2016-11-08 15:28:31 -0200

    Criação das classes para a implementação do formulário de levantamento.

--- wsgi/gde/app/models.py
+++ wsgi/gde/app/models.py
@@ -34,4 +34,77 @@ class Atividade(models.Model):
 
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    lotacao = models.ForeignKey(Setor, null=True)
+    setor = models.ForeignKey(Setor, null=True)
+
+    def __str__(self):
+        return self.user
+
+class Opcao(models.Model):
+    nome = models.CharField(max_length=20, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Elemento(models.Model):
+    nome = models.CharField(max_length=60, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Suporte(models.Model):
+    nome = models.CharField(max_length=60, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=60, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class RestricaoAcesso(models.Model):
+    descricao = models.CharField(max_length=200, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.descricao
+
+class FormaDocumental(models.Model):
+    nome = models.CharField(max_length=20, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Fase(models.Model):
+    nome = models.CharField(max_length=20, null=True, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Tipologia(models.Model):
+    setor = models.ForeignKey(Setor, related_name='setor', null=True)
+    usuario = models.ForeignKey(Usuario, related_name='usuario', null=True)
+    fase = models.ManyToManyField(Fase)
+    especieDocumental = models.ForeignKey(EspecieDocumental,related_name='especieDocumental', null=True)
+    finalidade = models.TextField(null=True, blank=False, unique=True)
+    nome = models.CharField(max_length=50, null=True, blank=False, unique=True)
+    identificação = models.CharField(max_length=50, null=True, blank=False, unique=True)
+    atividade = models.ForeignKey(Atividade, related_name='atividade', null=True)
+    elemento = models.ManyToManyField(Elemento, related_name='elemento')
+    suporte = models.ForeignKey(Suporte, related_name='suporte', null=True)
+    formaDocumental = models.ForeignKey(FormaDocumental, related_name='formaDocumental', null=True)
+    genero = models.ManyToManyField(Genero, related_name='genero')
+    anexo = models.ForeignKey(Opcao, related_name='anexo',null=True)
+    relacaoInterna = models.ForeignKey(Opcao, related_name='relacaoInterna', null=True)
+    relacaoExterna = models.ForeignKey(Opcao, related_name='relacaoExterna', null=True)
+    inicioAcumulo = models.DateField(null=True)
+    fimAcumulo = models.DateField(null=True)
+    quantidadeAcumulada = models.CharField(max_length=50, null=True, blank=False, unique=True)
+    embasamentoLegal = models.CharField(max_length=50, null=True, blank=False, unique=True)
+    informacaoOutrosDocumentos = models.ForeignKey(Opcao, related_name='informacaoOutrosDocumentos', null=True)
+    restricaoAcesso = models.ManyToManyField(RestricaoAcesso, related_name='restricaoAcesso')
+    riscoPerda = models.ForeignKey(Opcao, related_name='riscoPerda', null=True)
+
+    def __str__(self):
+        return 'setor:'+self.setor+'usuário:'+self.usuario+'fase:'+self.fase+'espécie:'+\
+               self.especie+'nome:'+self.nome
